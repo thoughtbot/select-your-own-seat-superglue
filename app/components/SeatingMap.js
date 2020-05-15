b6e8ef1c4405325014c891cec4bee5b77bd14750
Johny Ho <johny@thoughtbot.com>
2020-05-15 18:23:02 -0400

    Add map zoom controls to `seats/index`
    For comparison [see this commit]
    
    The Superglue version is lengthier than the commit above. Instead of just
    introducing HTML changes, this commit also introduces a React component to
    control zoom behavior and refactoring to introduce a `_seating_map.json.props`
    
    [commit]: https://github.com/seanpdoyle/select-your-own-seat/commit/fd394fc256780a9b81db73e6af1d5063629a235c

--- app/components/SeatingMap.js
+++ app/components/SeatingMap.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import svgPanZoom from 'svg-pan-zoom'
+import SvgZoomControls from './SvgZoomControls'
+
 const buildSectionElements = (sections) => {
   return sections.map((section, index) => {
     const seatElements = section.seats.map((seat) => (
@@ -31,7 +33,7 @@ const buildSectionElements = (sections) => {
 }
 
 export default class extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.svgRef = React.createRef()
   }
@@ -56,38 +58,41 @@ export default class extends React.Component {
     const sectionElements = buildSectionElements(sections)
 
     return(
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        className="syos-seat-map"
-        width="1600px"
-        height="1600px"
-        viewBox="0 0 1600 1600"
-        ref={this.svgRef}
-      >
-        <rect fill="none" x="0" y="0" width="1600" height="1600"></rect>
-        <svg style={{display: "none"}}>
-          <symbol
-            id="seat-icon-unselected"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <circle fill="#37b24d" r="12" cx="12" cy="12"></circle>
-            <circle fill="#ffffff" r="6" cx="12" cy="12"></circle>
-          </symbol>
+      <>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          className="syos-seat-map"
+          width="1600px"
+          height="1600px"
+          viewBox="0 0 1600 1600"
+          ref={this.svgRef}
+        >
+          <rect fill="none" x="0" y="0" width="1600" height="1600"></rect>
+          <svg style={{display: "none"}}>
+            <symbol
+              id="seat-icon-unselected"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <circle fill="#37b24d" r="12" cx="12" cy="12"></circle>
+              <circle fill="#ffffff" r="6" cx="12" cy="12"></circle>
+            </symbol>
 
-          <symbol
-            id="seat-icon-selected"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <circle fill="#37b24d" r="12" cx="12" cy="12"></circle>
-          </symbol>
+            <symbol
+              id="seat-icon-selected"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <circle fill="#37b24d" r="12" cx="12" cy="12"></circle>
+            </symbol>
+          </svg>
+          { sectionElements }
         </svg>
-        { sectionElements }
-       </svg>
+        <SvgZoomControls />
+      </>
     )
   }
 }
