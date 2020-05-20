97903bd7b78bc58e155a81436382bc45bf867f52
Johny Ho <johny@thoughtbot.com>
2020-05-19 22:23:06 -0400

    "Progressively" enhance Selection State
    For comparison [see this commit][commit]
    
    Note that "Progressively" is in quotes, because while this represents a small
    iterative change that uses UJS, it is not progressive enhancement in the rails
    sense.
    
    Much like an earlier commit, `Persist map across page loads`, Superglue does not
    have the same problem that the Stimulus version does.
    
    In Superglue, we just have to make sure the application has a new state, and React
    will declaratively make diffed changes to the DOM to reflect that state.
    
    Superglue does have a different problem which is relevant to this commit message.
    By default, links are just links, if we want to enhance them, we must do so
    explicitly. So this commit adds `data-sg-visit`:
      1. To cart removal
      2. To the add to cart from dialog
      2. To the seat clicks so SPA transition works
    
    [commit]: https://github.com/seanpdoyle/select-your-own-seat/commit/57347d3165acd7783c6dbf49e78e6f021ab3ff98

--- app/components/SeatDialog.js
+++ app/components/SeatDialog.js
@@ -69,7 +69,7 @@ export default class extends React.Component {
             </div>
 
             <div className="syos-inline-stack__item" >
-              <ButtonTo {...seatSelectionForm} />
+              <ButtonTo {...seatSelectionForm} data-sg-visit={true}/>
             </div>
           </div>
         </footer>
