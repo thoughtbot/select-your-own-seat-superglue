2e600900231d6d6ccb1282a3a85d85409e14798a
Johny Ho <johny@thoughtbot.com>
2021-07-11 20:27:42 -0400

    Render Seats for a Venue with SVG elements
    For comparison [see this commit][commit]
    
    There's nothing exciting to show off in this commit, but that's also the
    point.
    
    Superglue is really a multi-page application. We include all the content
    together on the same `index.json.props` just as we would when we're
    working with just a single `index.html.erb`. Then we modify the JSX markup to
    account for the new incoming props.
    
    Of note! The very next commit is a cherry-pick of the test on the original
    select-your-own-seat branch. There were no additional changes to make the
    tests work!
    
    [commit]: https://github.com/seanpdoyle/select-your-own-seat/commit/216b59c57382d8d091ee380dac22779bce0f03f3

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,2 +1,15 @@
 class SeatsController < ApplicationController
+  before_action :use_jsx_rendering_defaults
+
+  def index
+    venue = Venue.find_by!(slug: params[:venue_id])
+    floor = venue.floors.find_by!(slug: params[:floor_id])
+    sections = floor.sections.includes(:seats)
+
+    render(locals: {
+      venue: venue,
+      floor: floor,
+      sections: sections,
+    })
+  end
 end
