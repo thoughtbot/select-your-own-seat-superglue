6eda487673ed9e73a826385866aa48eb594cc3d5
Johny Ho <johny@thoughtbot.com>
2023-09-26 22:32:26 -0400

    Switch floors from map
    For comparison [see this commit][commit]
    
    The commit above adds `active_link_to`. A gem that adds classes to links if
    they share the same path with the current_url.
    
    We can use the gem too! This commit uses a different helper from the same
    library to achieve the same effect.
    
    [commit]: https://github.com/seanpdoyle/select-your-own-seat/commit/1f53ca717a6c3ab1f4e7219098f9c74101b1daf8

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ gem 'sprockets-rails', :require => 'sprockets/railtie'
 gem 'superglue', path: '/Users/johnyho/superglue/superglue_rails'
 
 gem "form_props", "~> 0.0.3"
+gem 'active_link_to'
---
+++ app/components/FloorSwitcher.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default class extends React.Component {
+  render() {
+    const {
+      floors,
+    } = this.props
+
+    return (
+      <nav className="syos-frame__floor-nav syos-floor-nav">
+        <div className="syos-floor-nav__header">
+          <p className="syos-u-margin-bottom-3 syos-u-font-weight-bold">
+            Floors
+          </p>
+        </div>
+
+        <div className="syos-floor-nav__diagram-wrapper">
+          <ul className="syos-block-stack">
+            {
+              floors.map(({path, name, className}) => (
+                <li className="syos-block-stack__item" key={path}>
+                  <a href={path} className={className}>{name}</a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
